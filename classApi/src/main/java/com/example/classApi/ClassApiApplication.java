ccb13848c8a5387dbe88800a599ce924bd8910a3
abhishekgl2710 <abhishekgl2710@gmail.com>
2022-03-01 10:42:04 +0530

    First Commit to add project

---
+++ classApi/src/main/java/com/example/classApi/ClassApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.classApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassApiApplication.class, args);
+		System.out.println("Spring boot Started");
+	}
+
+}
